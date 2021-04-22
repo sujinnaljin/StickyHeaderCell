c11e45b1ca2d484e6d268805cd8fae09bedd36ee
sujin <sujikang@ebay.com>
2021-04-22 19:34:34 +0900

    Create initial project

---
+++ StickyHeaderCell/StickyHeaderCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyHeaderCell
+//
+//  Created by Kang, Su Jin (강수진) on 2021/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

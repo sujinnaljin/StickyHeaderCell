f6d9d6ad918b6c799ba14fa435d2b55131f85e25
sujin <sujikang@ebay.com>
2021-04-22 19:45:37 +0900

    Make collectionView and set dataSource & delegate

---
+++ StickyHeaderCell/StickyHeaderCell/MyCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCell.swift
+//  StickyHeaderCell
+//
+//  Created by Kang, Su Jin (강수진) on 2021/04/22.
+//
+
+import UIKit
+
+class MyCell: UICollectionViewCell {
+    static let cellId = "MyCell"
+}
--- StickyHeaderCell/StickyHeaderCell/ViewController.swift
+++ StickyHeaderCell/StickyHeaderCell/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- IBOutlet
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //MARK:- Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.register(MyCell.self, forCellWithReuseIdentifier: MyCell.cellId)
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-
 }
 
+//MARK:- UICollectionViewDelegate, UICollectionViewDataSource
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCell.cellId, for: indexPath as IndexPath) as! MyCell
+        cell.backgroundColor = .red
+        return cell
+    }
+}
